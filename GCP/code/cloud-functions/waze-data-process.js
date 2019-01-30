aed50db0a9699fca489d0e6efd30e358b4d37c62
Jose Araneta <jose@thinkingmachin.es>
2019-01-30 15:20:24 +0800

    add cloud function to process data

---
+++ GCP/code/cloud-functions/waze-data-process.js
@@ -0,0 +1,76 @@
+const waze_data_types = ['alerts', 'irregularities', 'jams']
+const BQ_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS';
+const WAZE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss:SSS';
+const moment = require('moment');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+
+function convert_time_format(datestring, format){
+  const output_format = format || BQ_TIME_FORMAT;
+  const dt = moment(datestring, WAZE_TIME_FORMAT);
+  return dt.format(output_format);
+}
+
+
+function write_buffer_to_gcs(filename, bucket_name, buffer){
+  console.log(`writing ${filename} to ${bucket_name}`)
+  const bucket = storage.bucket(bucket_name);
+  let file = bucket.file(filename);
+  const stream = file.createWriteStream({
+    resumable: false
+  });
+
+  stream.on('error', (err) => {
+    next(err);
+    console.log(err);
+  });
+
+  stream.end(buffer);
+}
+
+
+exports.processData = function(event, callback){
+  const bucket_name = process.env.DATA_BUCKET;
+  const file = event.data;
+
+  console.log('file : ');
+  console.log(file);
+  
+  const bucket = storage.bucket(bucket_name);
+  console.log(file.name)
+  bucket.file(file.name).download().then((data) => {
+    let contents = data[0];  
+    console.log('contents : ');
+    console.log(contents);
+    let raw_data = JSON.parse(contents.toString('utf8'));
+    console.log('parsed contents : ');
+    console.log(raw_data);
+    let startTimeMillis = raw_data['startTimeMillis'];
+    let endTimeMillis = raw_data['endTimeMillis'];
+    let startTime = convert_time_format(raw_data['startTime']);
+    let endTime = convert_time_format(raw_data['endTime']);
+    waze_data_types.forEach(data_type =>{
+      console.log(`trying data_type:${data_type}`);
+      if (!(data_type in raw_data)){
+        return;
+      }
+      let arr = [];
+      let data = raw_data[data_type];
+      data.forEach(obj => { //"push in" start and end time to each record
+        let newObj = JSON.parse(JSON.stringify(obj)); //copy data to be safe
+        newObj['startTimeMillis'] = startTimeMillis;
+        newObj['endTimeMillis'] = endTimeMillis;
+        newObj['startTime'] = startTime;
+        newObj['endTime'] = endTime;
+        arr.push(newObj);
+      });
+      console.log('attempting to write to GCS');
+      write_buffer_to_gcs(`${data_type}_${convert_time_format(startTime, 'YYYY_MM_DD_HH_mm_ss_SSS')}.json`, 
+        process.env.PROCESSED_DATA_BUCKET, Buffer.from(arr.map((x) => JSON.stringify(x)).join('\n')));
+    });
+    callback();
+  }).catch(err => {
+    console.log('An error while processing the feed data.')
+    throw err;
+  }); 
+}
--- GCP/main.tf
+++ GCP/main.tf
@@ -5,12 +5,34 @@ resource "google_storage_bucket" "bucket" {
   storage_class = "REGIONAL"
 }
 
+resource "google_storage_bucket" "processed_bucket" {
+  name          = "${var.waze_processed_bucket_name}"
+  provider      = "google"
+  location      = "${var.bucket_location}"
+  storage_class = "REGIONAL"
+}
+
 data "archive_file" "http_trigger" {
   type        = "zip"
   output_path = "${path.module}/code/cloud-functions/waze-data-download.zip"
 
   source {
     content  = "${file("${path.module}/code/cloud-functions/waze-data-download.js")}"
+    filename = "index.js"
+  }
+
+  source {
+    content  = "${file("${path.module}/code/cloud-functions/package.json")}"
+    filename = "package.json"
+  }
+}
+
+data "archive_file" "process_data" {
+  type        = "zip"
+  output_path = "${path.module}/code/cloud-functions/waze-data-process.zip"
+
+  source {
+    content  = "${file("${path.module}/code/cloud-functions/waze-data-process.js")}"
     filename = "index.js"
   }
 
@@ -27,9 +49,15 @@ resource "google_storage_bucket_object" "waze-data-download-function" {
   depends_on = ["data.archive_file.http_trigger"]
 }
 
-resource "google_cloudfunctions_function" "function" {
+resource "google_storage_bucket_object" "waze-data-process-function" {
+  name       = "waze-data-process.zip"
+  bucket     = "${google_storage_bucket.processed_bucket.name}"
+  source     = "${data.archive_file.process_data.output_path}"
+  depends_on = ["data.archive_file.process_data"]
+}
+
+resource "google_cloudfunctions_function" "download-function" {
   name                  = "download-function"
-  provider              = "google"
   description           = "waze download function"
   available_memory_mb   = 128
   source_archive_bucket = "${google_storage_bucket.bucket.name}"
@@ -45,7 +73,34 @@ resource "google_cloudfunctions_function" "function" {
 
   environment_variables {
     DATA_BUCKET           = "${var.waze_raw_bucket_name}"
-    PROCESSED_DATA_BUCKET = "${var.processed_bucket_name}"
+    PROCESSED_DATA_BUCKET = "${var.waze_processed_bucket_name}"
+    WAZEDATAURL           = "${var.waze_data_url}"
+  }
+}
+
+resource "google_cloudfunctions_function" "process-function" {
+  name                  = "process-function"
+  description           = "lightly process waze feed data"
+  available_memory_mb   = 128
+  source_archive_bucket = "${google_storage_bucket.processed_bucket.name}"
+  source_archive_object = "${google_storage_bucket_object.waze-data-process-function.name}"
+  timeout               = 60
+  entry_point           = "processData"
+  provider              = "google"
+
+  # see https://github.com/terraform-providers/terraform-provider-google/issues/2409 for notes on the `event_trigger` block
+  event_trigger {
+    event_type = "providers/cloud.storage/eventTypes/object.change"
+    resource   = "${google_storage_bucket.bucket.name}"
+  }
+
+  labels {
+    my-label = "waze-processor"
+  }
+
+  environment_variables {
+    DATA_BUCKET           = "${var.waze_raw_bucket_name}"
+    PROCESSED_DATA_BUCKET = "${var.waze_processed_bucket_name}"
     WAZEDATAURL           = "${var.waze_data_url}"
   }
 }
